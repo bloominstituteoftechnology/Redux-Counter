d7b4bde5dc32b58f2063297a1ec706cca1ae20b3
aaren-george <aarengeorge@me.com>
2017-08-03 22:45:14 -0400

    Redux mini sprint

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -22,11 +22,11 @@ class Counter extends Component {
             <p>
                 Clicked: {this.props.count} times
                 {" "}
-                <button onClick={() => /* Fill me in */ }>
+                <button onClick={this.props.increment}>
                     +
                 </button>
                 {" "}
-                <button onClick={() => /* Fill me in */ }>
+                <button onClick={this.props.decrement}>
                     -
                 </button>
                 {" "}
@@ -44,8 +44,8 @@ class Counter extends Component {
     }
 }
 
-// The mapStateToProps function specifies which portion of the 
-// state tree this component needs to receive. In this case, 
+// The mapStateToProps function specifies which portion of the
+// state tree this component needs to receive. In this case,
 // since our redux store is only storing the value of the count,
 // this component receives the whole state. In a more complex
 // redux application, though, it would receive only the relevant
