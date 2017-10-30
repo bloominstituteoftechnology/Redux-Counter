f2e81a40dd95e7bb6eddf0c951f874517a1d2c0b
Sondro <memoiran@hotmail.com>
2017-10-30 15:03:32 -0700

    Init

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -9,9 +9,11 @@ export default (count = 0, action) => {
     switch (action.type) {
         case INCREMENT:
             // Fill in the body of this case
+            return ++count;
         case DECREMENT:
             // Fill in the body of this case
+            return --count;
         default:
-            return count;
+            return count; 
     }
 };
