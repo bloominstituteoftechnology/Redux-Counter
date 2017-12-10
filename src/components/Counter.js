600ae80f9ce0aa0c3cfb093868f2a9182b0e7b23
Roarka <designerexpert@gmail.com>
2017-12-09 20:00:07 -0500

    Sprint Completed minor issues compiling errors came up tho

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,15 +3,15 @@ import { connect } from 'react-redux';
 import { increment, decrement } from '../actions';
 
 class Counter extends Component {
-    incrementIfOdd = () => {
-        // Extra: Implement an increment function that
-        // only increments if the counter value is odd
-    };
+    // incrementIfOdd = () => {
+    //     if (this.props.count % 2 !== 0) {
+    //         this.props.increment();
+    //     }
+    // };
 
-    incrementAsync = () => {
-        // Extra: Implement an increment function that
-        // increments after waiting for one second
-    };
+    // incrementAsync = () => {
+    //     setTimeout(() => this.props.increment(), 1000);
+    // };
 
     render() {
         // Fill in the two button onClick methods
@@ -21,23 +21,36 @@ class Counter extends Component {
             <p>
                 Clicked: {this.props.count} times
                 {" "}
-                <button onClick={() => /* Fill me in */ }>
+                <button onClick={() => {
+                    /* Fill me in */
+                    this.props.increment();
+                    } }>
                     +
                 </button>
                 {" "}
-                <button onClick={() => /* Fill me in */ }>
+                <button onClick={() => {
+                    /* Fill me in */
+                    this.props.decrement();
+                    } }>
                     -
                 </button>
                 {" "}
-                 {/* Uncomment these button tags if you got
-                around to implementing the extra credit functions */}
                 {/* <button onClick={this.incrementIfOdd}>
+                    Increment if odd
+                </button> */}
+                <button onClick={() => {
+                    if (this.props.count % 2 !== 0) {
+                        this.props.increment();
+                    }
+                }}>
                     Increment if odd
                 </button>
                 {" "}
-                <button onClick={this.incrementAsync}>
+                <button onClick={() => {
+                    setTimeout(() => this.props.increment(), 1000);
+                }}>
                     Increment async
-                </button>  */}
+                </button>
             </p>
         );
     }
