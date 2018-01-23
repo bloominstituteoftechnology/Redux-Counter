07e5c9bbf3749077c5b8213796efe2d594b2bb55
Elly <ellyhahn@gmail.com>
2018-01-22 18:32:18 -0800

    Elly - redux mini commit

--- immutable.js
+++ immutable.js
@@ -15,6 +15,14 @@
   Be sure to console.log() both references to ensure that they are pointing to different objects.
 */
 
+const person = {
+  name: Ben,
+  age: 30, 
+  hometown: Madison, AL,
+};
+
+const newPerson = Object.assign({}, person, {name: 'Jake', age: '30'});
+
 console.log(person);
 console.log(newPerson);
 
@@ -23,6 +31,10 @@ console.log(newPerson);
   Create a separate array of `superVillains`
   Concatenate the two of them together using `.concat()`
 */
+
+const superHeroes = ['Iron Man', 'Black Panter', 'Moon Knight'];
+const superVillains = ['The Joker', 'Thanos', 'Brainiac'];
+const together = superHeroes.concat(superVillains);
 
 console.log(superHeroes);
 console.log(superVillains);
@@ -35,6 +47,16 @@ console.log(combinedSupers);
   Create a new object `studentAverage` using Object.assign() from the given original object.
   The new object should filter out any scores that are < 34 Array.filter should be used here.
 */
+
+const studentScores = {
+  name: 'Elly',
+  age: 36, 
+  score:[49, 21, 50],
+}
+
+const studentAverage = Object.assign({}, studentScores.fiter((val) => {
+  return studentScores.score < 34;
+}));
 
 console.log(studentScores);
 console.log(studentAverage);
