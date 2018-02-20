35b8cfa2e824c9a898e9939c06b2d4b0ca0c580b
Arkoma <aaron@adburk.net>
2018-02-20 12:53:33 -0600

    initial commit - working counter

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,10 +10,16 @@ export const DECREMENT = 'DECREMENT';
 // is left to the reducer(s).
 
 export const increment = () => {
+   return {
+       type: INCREMENT
+   }
    // Fill in this function 
 };
 
 
 export const decrement = () => {
-   // Fill in this function 
+   return {
+       type: DECREMENT
+   }
+    // Fill in this function 
 };
