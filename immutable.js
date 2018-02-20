ef320e4387a494c4de6182bcc79d4f8c4ede4ece
ashleijo <shuryjo@gmail.com>
2018-02-20 15:53:01 -0500

    completed immutable.js

--- immutable.js
+++ immutable.js
@@ -8,12 +8,22 @@
 */
 
 /* Problem 1
-  Create an object called `person`. 
+  Create an object called `person`.
   It should have a name property, age and hometown.
   Create a `newPerson` object from `person` using Object.assign.
   Lets say that `newPerson` has the same hometown, but different name and age.
   Be sure to console.log() both references to ensure that they are pointing to different objects.
 */
+
+const person = {
+  name: 'Ashlei',
+  age: 28,
+  hometown: 'Detroit'
+};
+
+const newPerson = Object.assign({}, person);
+newPerson.name = 'Star';
+newPerson.age = 30;
 
 console.log(person);
 console.log(newPerson);
@@ -24,6 +34,10 @@ console.log(newPerson);
   Concatenate the two of them together using `.concat()`
 */
 
+const superHeroes = ['Superman','Batman','hyperman'];
+const superVillains = ['Cruella', 'Joker', 'Darth Vader'];
+const combinedSupers = superHeroes.concat(superVillains);
+
 console.log(superHeroes);
 console.log(superVillains);
 console.log(combinedSupers);
@@ -31,10 +45,19 @@ console.log(combinedSupers);
 /* Problem 3
   Create an object `studentScores` with a 'name', 'age' and 'scores'
   Where 'name' is a string, 'age' is a number and 'scores' is an array of numbers
-  The numbers for scores should be between 1 - 50. 
+  The numbers for scores should be between 1 - 50.
   Create a new object `studentAverage` using Object.assign() from the given original object.
   The new object should filter out any scores that are < 34 Array.filter should be used here.
 */
+
+const studentScores = {
+  name: 'Ashlei',
+  age: '23',
+  scores: [50, 45, 30, 1, 50, 50]
+}
+
+const studentAverage = Object.assign({}, studentScores);
+studentAverage.scores = Object.assign(studentScores.scores.filter(word => word > 34));
 
 console.log(studentScores);
 console.log(studentAverage);
