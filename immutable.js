eecb9c5314424b6cd62e53d1308c73ea2d884a73
Michael Marshal <mikemarshalman@gmail.com>
2018-02-20 15:16:39 -0600

    immutable finished

--- immutable.js
+++ immutable.js
@@ -1,5 +1,5 @@
 /*
-  In this file you're going to working with some objects and arrays.
+  In this file you're going to be working with some objects and arrays.
   Instead of mutating the data from within an object/array itself,
     you're going to be using immutible techniques
   Be sure to **TEST** out your code in an editor/browser REPL of your choice
@@ -8,12 +8,28 @@
 */
 
 /* Problem 1
-  Create an object called `person`. 
+  Create an object called `person`.
   It should have a name property, age and hometown.
   Create a `newPerson` object from `person` using Object.assign.
   Lets say that `newPerson` has the same hometown, but different name and age.
   Be sure to console.log() both references to ensure that they are pointing to different objects.
 */
+
+const person = {
+  name: 'Michael',
+  age: 24,
+  hometown: 'Chicago',
+};
+
+// let newPerson = {
+//   ...person,
+//   name: 'Eric',
+//   age: 28,
+// };
+
+const newPerson = Object.assign({}, person);
+newPerson.name = 'Eric';
+newPerson.age = 28;
 
 console.log(person);
 console.log(newPerson);
@@ -24,6 +40,11 @@ console.log(newPerson);
   Concatenate the two of them together using `.concat()`
 */
 
+const superHeroes = ['Batman', 'Superman', 'Spiderman'];
+const superVillains = ['Joker', 'Lex Luther', 'Venom'];
+
+const combinedSupers = superHeroes.concat(superVillains);
+
 console.log(superHeroes);
 console.log(superVillains);
 console.log(combinedSupers);
@@ -31,10 +52,21 @@ console.log(combinedSupers);
 /* Problem 3
   Create an object `studentScores` with a 'name', 'age' and 'scores'
   Where 'name' is a string, 'age' is a number and 'scores' is an array of numbers
-  The numbers for scores should be between 1 - 50. 
+  The numbers for scores should be between 1 - 50.
   Create a new object `studentAverage` using Object.assign() from the given original object.
   The new object should filter out any scores that are < 34 Array.filter should be used here.
 */
+
+const studentScores = {
+  name: 'Eric',
+  age: 28,
+  scores: [48, 22, 1, 23, 7, 12, 23, 8, 43, 50],
+};
+
+const studentAverage = Object.assign({}, studentScores);
+studentAverage.scores = studentAverage.scores.filter(each => {
+  return each >= 34;
+});
 
 console.log(studentScores);
 console.log(studentAverage);
