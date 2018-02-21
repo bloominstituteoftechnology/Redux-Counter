68d786e399deadadd80d96093f1aeba1645fe9d0
Dan-Lara <endtrippy@gmail.com>
2018-02-20 18:00:29 -0700

    Mini/Stretch Complete

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,13 +12,11 @@ export const DECREMENT = "DECREMENT";
 export const increment = () => {
   return {
     type: INCREMENT,
-    payload: 1,
     };
 };
 
 export const decrement = () => {
   return {
     type: DECREMENT,
-    payload: 1,
     };
 };
