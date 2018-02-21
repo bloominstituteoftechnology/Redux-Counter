7d22c21b262ba2a08bf08ece04a39287fc94cab5
Eric <erichech@gmail.com>
2018-02-20 20:12:24 -0500

    null

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,11 +12,11 @@ export const DECREMENT = 'DECREMENT';
 export const increment = () => {
   return {
     type: INCREMENT,
-  }
+  };
 };
 
 export const decrement = () => {
   return {
     type: DECREMENT,
-  }};
-  
+  };
+};
