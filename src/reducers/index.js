ae5a1460d9bc88a32d83c5478ed3d3daba8bd06d
KJ Sourcing <tmkogwork@gmail.com>
2018-03-19 14:52:08 -0700

    do the easy file first, add and subtract, yay

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,13 +4,13 @@ import { INCREMENT, DECREMENT } from '../actions';
 // increment and decrement. It receives the state
 // of our redux store, along with an action created
 // by our action creator. What does the reducer
-// need to do the count in each case?
+// need to do the count in each case? :: add and subtract
 export default (count = 0, action) => {
     switch (action.type) {
         case INCREMENT:
-            // Fill in the body of this case
+            return ++count;
         case DECREMENT:
-            // Fill in the body of this case
+            return --count;
         default:
             return count;
     }
