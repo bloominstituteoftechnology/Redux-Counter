4c14eea8d24247cb5ded479ba5a6551114ad153a
Carlos Andrade <carlos.andrade@outlook.com>
2018-03-19 19:20:47 -0400

    Fixed a bug, SUPER COMPLETED

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -9,10 +9,12 @@ export default (count = 0, action) => {
     switch (action.type) {
         case INCREMENT:
             // Fill in the body of this case
-            {return count++ }        
+            {count++ }
+            return count;        
         case DECREMENT:
             // Fill in the body of this case
-            {return count-- }           
+            {count-- }
+            return count;           
         default:
             return count;
     }
