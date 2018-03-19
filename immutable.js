a6916d6be0eb91fa16cf9122d296815f3c83c4aa
Jeremy Jones <jeremyjones@Macintosh-58b035f2f989.local>
2018-03-19 13:24:23 -0700

    immutable.js problems completed

--- immutable.js
+++ immutable.js
@@ -15,6 +15,14 @@
   Be sure to console.log() both references to ensure that they are pointing to different objects.
 */
 
+const person = {
+  name: 'Jeremy',
+  age: 34,
+  hometown: 'New Orleans'
+};
+
+const newPerson = Object.assign({}, person, { name: 'Mike', age: 40 });
+
 console.log(person);
 console.log(newPerson);
 
@@ -23,6 +31,11 @@ console.log(newPerson);
   Create a separate array of `superVillains`
   Concatenate the two of them together using `.concat()`
 */
+
+const superHeroes = ['Superman', 'Batman', 'Spiderman', 'Flash'];
+const superVillains = ['Lex Luthor', 'Joker', 'Green Goblin', 'Professor Zoom'];
+
+const combinedSupers = superHeroes.concat(superVillains);
 
 console.log(superHeroes);
 console.log(superVillains);
@@ -35,6 +48,20 @@ console.log(combinedSupers);
   Create a new object `studentAverage` using Object.assign() from the given original object.
   The new object should filter out any scores that are < 34 Array.filter should be used here.
 */
+
+const studentScores = {
+  name: 'John',
+  age: 22,
+  scores: [45, 42, 29, 32, 40]
+};
+
+function isBigEnough(value) {
+  return value > 34;
+}
+
+const studentAverage = Object.assign({}, studentScores, {
+  scores: studentScores.scores.filter(isBigEnough)
+});
 
 console.log(studentScores);
 console.log(studentAverage);
