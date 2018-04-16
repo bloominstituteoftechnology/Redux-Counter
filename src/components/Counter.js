f02137d9e366db1a20115224b090534e0a632ff7
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2018-04-16 15:10:57 -0600

    Added increment async functionality

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -12,6 +12,7 @@ class Counter extends Component {
     incrementAsync = () => {
         // Stretch Problem: Implement an increment function that
         // increments after waiting for one second
+        setTimeout(this.props.increment, 1000);
     };
 
     render() {
