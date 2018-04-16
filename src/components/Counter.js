86ba8d1e123fd884a2b571561860491f67b53a89
Kyle Kauzlarich <kauzlarich.kyle@gmail.com>
2018-04-16 13:57:29 -0700

    fixed line 3 error on compile

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
-import { increment, decrement, DECREMENT, INCREMENT } from '../actions';
+import { increment, decrement } from '../actions';
 
 class Counter extends Component {
     incrementIfOdd = () => {
