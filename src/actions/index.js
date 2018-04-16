d523753104c944fb660f3f45fd15c4b0c9c88e03
Paul Fang <paulfang@pauls-mbp.home>
2018-04-16 13:41:06 -0700

    setting up index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,9 +10,9 @@ export const DECREMENT = 'DECREMENT';
 // is left to the reducer(s).
 
 export const increment = () => {
-  console.log('increment');
+  return ({type: 'INCREMENT'})
 };
 
 export const decrement = () => {
-  console.log('decrement');
+  return ({type: 'DECREMENT'})
 };
