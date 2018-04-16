0837c4699a2781ba4936b5755f1c1f2482191667
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-04-16 16:21:22 -0400

    Set up counter. Step 1, filled in the render to set up functions in Counter.js, define the functions in index.js in actions, implement the action in index.js in reducers

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,6 +3,9 @@ import { connect } from 'react-redux';
 import { increment, decrement } from '../actions';
 
 class Counter extends Component {
+    constructor(props) {
+        super(props);
+    }
     incrementIfOdd = () => {
         // Stretch Problem: Implement an increment function that
         // only increments if the counter value is odd
@@ -20,10 +23,10 @@ class Counter extends Component {
         return (
             <p>
                 Clicked: {this.props.count} times
-                <button onClick={() => /* Fill me in */ }>
+                <button onClick={() => this.props.increment()}>
                     +
                 </button>
-                <button onClick={() => /* Fill me in */ }>
+                <button onClick={() => this.props.decrement()}>
                     -
                 </button>
                  {/* Uncomment these button tags if you got
