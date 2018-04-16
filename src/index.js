a2f30a95c303828bf552045ac20c044140e27f63
Justin Hong <jhong2009@gmail.com>
2018-04-16 13:29:05 -0700

    in reducers/index.js: added increment/decrement logic

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import Counter from './components/Counter';
-import counter from './reducers';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import Counter from "./components/Counter";
+import counter from "./reducers";
 
 // This line instantiates our central Redux store.
 // The `createStore` function receives the reducer
 // that is responsible for updating the store, along
 // with any initial state that we may want the store
 // to start out with (which is none in this case).
-const store = createStore(counter);
+const store = createStore(
+	counter,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 // Here, we wrap our main React component inside of
 // Provider tags, which come from the react-redux package.
@@ -18,8 +21,8 @@ const store = createStore(counter);
 // is passing its state to. The Provider component is also
 // where the store "lives".
 ReactDOM.render(
-  <Provider store={store}>
-    <Counter />
-  </Provider>,
-  document.getElementById('root')
+	<Provider store={store}>
+		<Counter />
+	</Provider>,
+	document.getElementById("root")
 );
