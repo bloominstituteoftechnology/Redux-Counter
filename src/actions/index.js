5b25f4c33c503878495eb77820a0bac526c6d74d
Anthony <anthony@anthonys-mbp.lan>
2018-04-16 17:09:12 -0400

    finished project 1

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 export const INCREMENT = 'INCREMENT';
 export const DECREMENT = 'DECREMENT';
 
+
 // Our action creators will return
 // an action packet that our reducer will
 // receive. What does the action packet look like?
@@ -9,10 +10,17 @@ export const DECREMENT = 'DECREMENT';
 // logic of updating the central Redux store. That
 // is left to the reducer(s).
 
+// const store = createStore(counterIncrease, 0)
 export const increment = () => {
   // Fill in this function
+  return {
+    type: INCREMENT
+  }
 };
 
 export const decrement = () => {
   // Fill in this function
+  return {
+    type: DECREMENT
+  }
 };
--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ import counter from './reducers';
 // that is responsible for updating the store, along
 // with any initial state that we may want the store
 // to start out with (which is none in this case).
-const store = createStore(counter);
 
+const store = createStore(counter);
 // Here, we wrap our main React component inside of
 // Provider tags, which come from the react-redux package.
 // This is needed because the store needs to know where it
@@ -23,3 +23,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+
