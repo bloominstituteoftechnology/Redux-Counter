4e202d74ff090d4d75efbb13af2745ddc0f419fe
Natalie Schennum <natalieschennum@gmail.com>
2018-04-16 13:37:04 -0700

    Completed Action Export Statement; completed Reducer Switch Statement; completed component Click functions

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -8,9 +8,9 @@ import { INCREMENT, DECREMENT } from '../actions';
 export default (count = 0, action) => {
   switch (action.type) {
     case INCREMENT:
-    // Fill in the body of this case
+      return count +1; // Fill in the body of this case
     case DECREMENT:
-    // Fill in the body of this case
+      return count -1; // Fill in the body of this case
     default:
       return count;
   }
