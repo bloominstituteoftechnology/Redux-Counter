4ef61c5b0ed755d8d507c9ec81e3ec12c5adb747
Rachelle De Jesus Pestanas <rachelle.pestanas@gmail.com>
2018-04-16 17:41:04 -0500

    completed reducers/index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -8,9 +8,11 @@ import { INCREMENT, DECREMENT } from '../actions';
 export default (count = 0, action) => {
   switch (action.type) {
     case INCREMENT:
+    return count + 1;
     // Fill in the body of this case
     case DECREMENT:
     // Fill in the body of this case
+    return count - 1;
     default:
       return count;
   }
