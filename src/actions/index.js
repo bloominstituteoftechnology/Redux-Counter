7d2245e04caefa3da31471435a0511e8bbcfaf59
Mack <macknwandu@github.com>
2018-04-16 12:01:58 -0500

    increments and decrements accordingly

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 export const INCREMENT = 'INCREMENT';
 export const DECREMENT = 'DECREMENT';
 
+
 // Our action creators will return
 // an action packet that our reducer will
 // receive. What does the action packet look like?
@@ -11,8 +12,10 @@ export const DECREMENT = 'DECREMENT';
 
 export const increment = () => {
   // Fill in this function
+  return {type: 'INCREMENT'};
 };
 
 export const decrement = () => {
   // Fill in this function
+  return {type: 'DECREMENT'};
 };
