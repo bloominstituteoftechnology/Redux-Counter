be1368caafaba078fcd6f8a7eec03499204a364c
Ilho Kim <ilhokim@Ilhoui-MacBook-Pro.local>
2018-04-16 13:45:21 -0700

    actions/index.js, reducers/index.js, Counter.js updated

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -8,9 +8,10 @@ import { INCREMENT, DECREMENT } from '../actions';
 export default (count = 0, action) => {
   switch (action.type) {
     case INCREMENT:
-    // Fill in the body of this case
+      // {count: count++}
+      return count++;
     case DECREMENT:
-    // Fill in the body of this case
+      return count--;
     default:
       return count;
   }
