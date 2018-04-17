026fcd7042e3bbf4a2b46d4bd3b86fe877321f6e
Justin M <jekm321@gmail.com>
2018-04-17 12:50:24 -0400

    fixed mutability in reducer index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -8,9 +8,9 @@ import { INCREMENT, DECREMENT } from '../actions';
 export default (count = 0, action) => {
   switch (action.type) {
     case INCREMENT:
-      return count + action.payload;
+      return count = count += action.payload;
     case DECREMENT:
-      return count - action.payload;
+      return count = count -= action.payload;
     default:
       return count;
   }
