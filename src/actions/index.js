2a0c2401ae504c75496dd7eeaac4baf1feaef977
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-05-14 13:36:43 -0700

    switch cases done

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,8 +10,9 @@ export const DECREMENT = 'DECREMENT';
 // is left to the reducer(s).
 
 export const increment = () => {
+  return { type: INCREMENT }
 };
 
 export const decrement = () => {
-  // Fill in this function
+  return { type: INCREMENT }
 };
