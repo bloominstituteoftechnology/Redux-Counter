4c323bd1e4b4db1c9b910ffb4b2e2b28b389cdb7
maxwashington <mnwashington18@gmail.com>
2018-05-14 14:22:22 -0500

    All Basic goals complete

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -20,10 +20,10 @@ class Counter extends Component {
         return (
             <p>
                 Clicked: {this.props.count} times
-                <button onClick={() => /* Fill me in */ }>
+                <button onClick={() => this.prop.increment(this.prop.count) }>
                     +
                 </button>
-                <button onClick={() => /* Fill me in */ }>
+                <button onClick={() => this.prop.decrement(this.prop.count) }>
                     -
                 </button>
                  {/* Uncomment these button tags if you got
