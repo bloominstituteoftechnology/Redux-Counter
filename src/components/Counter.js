8c5f83021db27752ea5a53d48425acee2d957398
Obo Agboghidi <oagboghi2@gmail.com>
2018-05-14 15:39:41 -0700

    Obo Agboghidi 2nd commit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -6,11 +6,18 @@ class Counter extends Component {
     incrementIfOdd = () => {
         // Stretch Problem: Implement an increment function that
         // only increments if the counter value is odd
+        console.log(this.props.count)
+        if(this.props.count % 2 !== 0){
+            return this.props.increment();
+        }
     };
 
     incrementAsync = () => {
         // Stretch Problem: Implement an increment function that
         // increments after waiting for one second
+        setTimeout(() =>{ 
+            return this.props.increment();
+        },1000)
     };
 
     render() {
@@ -28,12 +35,12 @@ class Counter extends Component {
                 </button>
                  {/* Uncomment these button tags if you got
                 around to implementing the extra credit functions */}
-                {/* <button onClick={this.incrementIfOdd}>
+                <button onClick={this.incrementIfOdd}>
                     Increment if odd
                 </button>
                 <button onClick={this.incrementAsync}>
                     Increment async
-                </button>  */}
+                </button>
             </p>
         );
     }
