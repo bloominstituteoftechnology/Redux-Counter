6d70ede005dab546bcd14332375b0c9dcb10e270
Derric-Mei <derrick.mei.168@gmail.com>
2018-05-14 15:36:11 -0500

    finished everything plus stretch!!!

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -9,8 +9,12 @@ export default (count = 0, action) => {
   switch (action.type) {
     case INCREMENT:
     // Fill in the body of this case
+      count += action.payload;
+      return count;
     case DECREMENT:
     // Fill in the body of this case
+      count --
+      return count;
     default:
       return count;
   }
