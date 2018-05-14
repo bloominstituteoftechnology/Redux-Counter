134cf1065862c63027d858cab00046c33217f05d
udykas <udykas@gmail.com>
2018-05-14 18:20:17 -0400

    Finished assignment

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -9,8 +9,10 @@ export default (count = 0, action) => {
   switch (action.type) {
     case INCREMENT:
     // Fill in the body of this case
+      return ++action.count;
     case DECREMENT:
     // Fill in the body of this case
+      return --action.count;
     default:
       return count;
   }
