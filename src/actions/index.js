908877377e0b64600be546ff3bb30b3ef92e5ced
Lavell Burton <taylormade800@gmail.com>
2018-05-14 13:19:38 -0700

    added counter function in index.js file

--- src/actions/index.js
+++ src/actions/index.js
@@ -11,8 +11,16 @@ export const DECREMENT = 'DECREMENT';
 
 export const increment = () => {
   // Fill in this function
+  let count = 0;
+  return function counted() {
+    return ++count;
+  }
 };
 
 export const decrement = () => {
   // Fill in this function
+  let count = 0;
+  return function counted() {
+    return --count;
+  }
 };
