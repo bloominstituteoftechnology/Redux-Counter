35611eb0e9fa1cdf915a13a5ccb40cb69b63f6c9
Herbert Hodgson <herberthodgson@hotmail.com>
2018-05-14 17:20:21 -0400

    Completed Counter Project

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -13,6 +13,16 @@ class Counter extends Component {
         // increments after waiting for one second
     };
 
+    increment = () => {
+        this.props.increment(this.count);
+    };
+
+    decrement = () => {
+        this.props.decrement(this.count);
+    }
+
+
+
     render() {
         // Fill in the two button onClick methods
         // Upon clicking these buttons, the count
@@ -20,10 +30,10 @@ class Counter extends Component {
         return (
             <p>
                 Clicked: {this.props.count} times
-                <button onClick={() => /* Fill me in */ }>
+                <button onClick={this.increment}>
                     +
                 </button>
-                <button onClick={() => /* Fill me in */ }>
+                <button onClick={this.decrement}>
                     -
                 </button>
                  {/* Uncomment these button tags if you got
