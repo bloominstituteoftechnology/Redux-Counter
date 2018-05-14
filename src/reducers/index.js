11699b773b927bd050c47a67ff7e63216425f314
jaklundun <govi.escudero@gmail.com>
2018-05-14 14:54:25 -0700

    Completed assignment tasks

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -10,10 +10,10 @@ export default (count = 0, action) => {
   switch (action.type) {
     case INCREMENT:
     // Fill in the body of this case
-      return count++
+      return ++action.count
     case DECREMENT:
     // Fill in the body of this case
-      return --count
+      return --action.count
     default:
       return count;
   }
