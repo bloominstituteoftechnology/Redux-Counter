3f98dea3c23ec989062cc314e22cad50a1e7851f
emortalix <emortalix@welcometoforever.io>
2018-05-14 15:26:37 -0400

    Redux Work

--- src/actions/index.js
+++ src/actions/index.js
@@ -11,8 +11,14 @@ export const DECREMENT = 'DECREMENT';
 
 export const increment = () => {
   // Fill in this function
+  return {
+    type: INCREMENT
+  }
 };
 
 export const decrement = () => {
   // Fill in this function
+  return {
+    type: DECREMENT
+  }
 };
