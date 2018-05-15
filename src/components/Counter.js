c81a998e76b1d5599d97d5abcfadd7ec2adad6a3
kammagik <kammagik@yahoo.com>
2018-05-14 17:06:31 -0700

    stretch goals

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,13 +4,23 @@ import { increment, decrement } from '../actions';
 
 class Counter extends Component {
     incrementIfOdd = () => {
+      if(this.props.count % 2 !=== 0) {
+      }
+
+      else {
+        console.log('even number!!')
+      }
         // Stretch Problem: Implement an increment function that
         // only increments if the counter value is odd
     };
 
     incrementAsync = () => {
+      setTimeout(()
+        this.props.increment();
+      }, 1000);
         // Stretch Problem: Implement an increment function that
         // increments after waiting for one second
+
     };
 
     render() {
@@ -20,27 +30,27 @@ class Counter extends Component {
         return (
             <p>
                 Clicked: {this.props.count} times
-                <button onClick={() => /* Fill me in */ }>
+                <button onClick={() => this.props.increment}>
                     +
                 </button>
-                <button onClick={() => /* Fill me in */ }>
+                <button onClick={() => this.props.decrement}>
                     -
                 </button>
-                 {/* Uncomment these button tags if you got
+                 { Uncomment these button tags if you got
                 around to implementing the extra credit functions */}
-                {/* <button onClick={this.incrementIfOdd}>
+                {<button onClick={this.incrementIfOdd}>
                     Increment if odd
                 </button>
                 <button onClick={this.incrementAsync}>
                     Increment async
-                </button>  */}
+                </button>  }
             </p>
         );
     }
 }
 
-// The mapStateToProps function specifies which portion of the 
-// state tree this component needs to receive. In this case, 
+// The mapStateToProps function specifies which portion of the
+// state tree this component needs to receive. In this case,
 // since our redux store is only storing the value of the count,
 // this component receives the whole state. In a more complex
 // redux application, though, it would receive only the relevant
