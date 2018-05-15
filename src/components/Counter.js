045795eacad3bc67fb9d94a91dfa6b37882cc6bc
Usman Javed <ujaved931@gmail.com>
2018-05-16 01:24:54 +0500

    Finished Stretch Added Async Method Added Odd Method

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,16 +1,22 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
 import { increment, decrement } from '../actions';
+import count from '../reducers'
 
 class Counter extends Component {
     incrementIfOdd = () => {
+        if (this.props.count % 2 !== 0) {
+            return this.props.increment(); 
+        }
         // Stretch Problem: Implement an increment function that
         // only increments if the counter value is odd
     };
 
     incrementAsync = () => {
-        // Stretch Problem: Implement an increment function that
-        // increments after waiting for one second
+        return setTimeout(() => {
+            return this.props.increment();
+        }, 1000);
+        
     };
 
     render() {
@@ -28,12 +34,12 @@ class Counter extends Component {
                 </button>
                  {/* Uncomment these button tags if you got
                 around to implementing the extra credit functions */}
-                {/* <button onClick={this.incrementIfOdd}>
+                <button onClick={this.incrementIfOdd}>
                     Increment if odd
                 </button>
                 <button onClick={this.incrementAsync}>
                     Increment async
-                </button>  */}
+                </button>  
             </p>
         );
     }
