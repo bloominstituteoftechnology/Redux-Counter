88f8635bd71e7cbb8478801bd20df4bcfc1a6016
Gabriel Duquette <gabrielduquette@gmail.com>
2018-07-09 16:04:31 -0400

    first commit

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -8,9 +8,9 @@ import { INCREMENT, DECREMENT } from '../actions';
 export default (count = 0, action) => {
   switch (action.type) {
     case INCREMENT:
-    // Fill in the body of this case
+      return Object.assign({}, count, { count++ });
     case DECREMENT:
-    // Fill in the body of this case
+      return Object.assign({}, count, { count-- });
     default:
       return count;
   }
