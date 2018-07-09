e7fe35c9eec82f850d7301646ea0fc407ab6e1c9
Erin Meredith <ekmeredith@att.net>
2018-07-09 17:02:10 -0400

    updated actions, reducers, Counter

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,9 +10,9 @@ export const DECREMENT = 'DECREMENT';
 // is left to the reducer(s).
 
 export const increment = () => {
-  // Fill in this function
+  type: INCREMENT
 };
 
 export const decrement = () => {
-  // Fill in this function
+  type: DECREMENT
 };
