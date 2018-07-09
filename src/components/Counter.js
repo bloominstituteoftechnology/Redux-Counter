183a0be5b297f4fb49ac6995afea40601f87b87a
Simon <Simon_Freelance@hotmail.com>
2018-07-09 15:50:59 -0400

    Added event handlers to buttons

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -20,10 +20,10 @@ class Counter extends Component {
         return (
             <p>
                 Clicked: {this.props.count} times
-                <button onClick={() => /* Fill me in */ }>
+                <button onClick={() => increment }>
                     +
                 </button>
-                <button onClick={() => /* Fill me in */ }>
+                <button onClick={() => decrement }>
                     -
                 </button>
                  {/* Uncomment these button tags if you got
