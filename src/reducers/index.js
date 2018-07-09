12f6e97ff48cd8672d2952c5b993e13efd64c82d
emaquino44 <prexy44@yahoo.com>
2018-07-09 12:12:11 -0700

    initial commit

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -9,9 +9,14 @@ export default (count = 0, action) => {
   switch (action.type) {
     case INCREMENT:
     // Fill in the body of this case
+
+    action
     case DECREMENT:
     // Fill in the body of this case
     default:
       return count;
   }
 };
+
+
+
