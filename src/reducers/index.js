61625ee8c0d64d8df07b9639a6887ddeb55aef81
Gabriel Duquette <gabrielduquette@gmail.com>
2018-07-09 16:55:34 -0400

    complete MVP and stretch

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -8,9 +8,9 @@ import { INCREMENT, DECREMENT } from '../actions';
 export default (count = 0, action) => {
   switch (action.type) {
     case INCREMENT:
-      return Object.assign({}, count, { count++ });
+      return count += 1;
     case DECREMENT:
-      return Object.assign({}, count, { count-- });
+      return count -= 1;
     default:
       return count;
   }
