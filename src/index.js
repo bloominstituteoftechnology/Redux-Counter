69f11d3b614e8578195638a0decac7b587a27c25
Luis <luis@Luiss-MacBook-Air.local>
2018-07-09 11:20:45 -0700

    inital commit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import Counter from './components/Counter';
-import counter from './reducers';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import Counter from './components/Counter'
+import counter from './reducers'
 
 // This line instantiates our central Redux store.
 // The `createStore` function receives the reducer
 // that is responsible for updating the store, along
 // with any initial state that we may want the store
 // to start out with (which is none in this case).
-const store = createStore(counter);
+const store = createStore(counter)
 
 // Here, we wrap our main React component inside of
 // Provider tags, which come from the react-redux package.
@@ -22,4 +22,4 @@ ReactDOM.render(
     <Counter />
   </Provider>,
   document.getElementById('root')
-);
+)
