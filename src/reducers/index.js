372c32f4a64911dcc82ac358bfdce192a7b02abe
Ari <ari@Nubias-iMac.local>
2018-07-10 10:37:21 -0700

    mvp

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -9,10 +9,10 @@ export default (count = 0, action) => {
   switch (action.type) {
     case INCREMENT:
     // Fill in the body of this case
-      return (count += action.payload);
+      return ++action.count;
     case DECREMENT:
     // Fill in the body of this case
-      return (count -= action.payload);
+      return --action.count
     default:
       return count;
   }
