40ddb0570325fafec94c2c433e9ac77f33035900
Sumayyah <sumayyah@zubairs-air.attlocal.net>
2018-07-16 16:33:21 -0700

    PR

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -8,9 +8,11 @@ import { INCREMENT, DECREMENT } from '../actions';
 export default (count = 0, action) => {
   switch (action.type) {
     case INCREMENT:
-    // Fill in the body of this case
+      // Fill in the body of this case
+      return { ...state, count: action.payload++ };
     case DECREMENT:
-    // Fill in the body of this case
+      // Fill in the body of this case
+      return { ...state, count: action.payload-- };
     default:
       return count;
   }
