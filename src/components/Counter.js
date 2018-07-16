dba0081ee018b96e4c50f6f760b50de11315bcbf
Katie Gorbell <katiegorbell@gmail.com>
2018-07-16 16:17:04 -0500

    working on incrementIfOdd

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -6,6 +6,12 @@ class Counter extends Component {
     incrementIfOdd = () => {
         // Stretch Problem: Implement an increment function that
         // only increments if the counter value is odd
+        if (this.props.store.count % 2 !== 0) {
+            return {
+                type: 'INCREMENT'
+            }
+        }
+
     };
 
     incrementAsync = () => {
@@ -26,14 +32,12 @@ class Counter extends Component {
                 <button onClick={() => this.props.decrement() }>
                     -
                 </button>
-                 {/* Uncomment these button tags if you got
-                around to implementing the extra credit functions */}
-                {/* <button onClick={this.incrementIfOdd}>
+                <button onClick={this.incrementIfOdd}>
                     Increment if odd
                 </button>
                 <button onClick={this.incrementAsync}>
                     Increment async
-                </button>  */}
+                </button> 
             </p>
         );
     }
