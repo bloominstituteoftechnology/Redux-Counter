ce7792d864ca724a14cd5b1b3740a20b4b00c098
Katie Gorbell <katiegorbell@gmail.com>
2018-07-16 16:49:05 -0500

    finished stretch goals

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -6,17 +6,17 @@ class Counter extends Component {
     incrementIfOdd = () => {
         // Stretch Problem: Implement an increment function that
         // only increments if the counter value is odd
-        if (this.props.store.count % 2 !== 0) {
-            return {
-                type: 'INCREMENT'
+        if (this.props.count % 2 !== 0) {
+                this.props.increment();
             }
-        }
-
     };
 
     incrementAsync = () => {
         // Stretch Problem: Implement an increment function that
         // increments after waiting for one second
+        setTimeout(() => {
+            this.props.increment();
+        } , 1000);
     };
 
     render() {
