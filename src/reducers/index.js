7c41bd88311cd63cbb6df4a5babd3280c39f4b2e
JacobLayton <jlayjlay3@gmail.com>
2018-07-16 15:46:04 -0700

    MVP complete

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -8,12 +8,16 @@ import { INCREMENT, DECREMENT } from '../actions';
 export default (count = 0, action) => {
   switch (action.type) {
     case INCREMENT:
-    return count + 1;
-    // Fill in the body of this case
+      return count = count + 1;
     case DECREMENT:
-    return count - 1;
-    // Fill in the body of this case
+      return count = count - 1;
     default:
       return count;
   }
 };
+
+
+//You might need to make this reducer a pure funtion byt making those INC and DEC cases return something
+//like Object.assign({}, state, { name: action.payload });
+
+//45 minutes into the lecture
