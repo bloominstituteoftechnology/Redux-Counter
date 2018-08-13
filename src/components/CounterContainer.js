c566dc9cfe561431617f60af3094f5d343a31f85
johnoro <officialjohnorourke@gmail.com>
2018-08-13 15:23:10 -0400

    Finished project in full.

---
+++ src/components/CounterContainer.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { increment, decrement } from "../actions";
+import Counter from "./Counter";
+
+class CounterContainer extends Component {
+  incrementIfOdd = () => {
+    if (this.props.count % 2 !== 0) {
+      this.props.increment();
+    }
+  };
+
+  incrementAsync = () => {
+    setTimeout(this.props.increment, 1000);
+  };
+
+  render() {
+    return (
+      <Counter
+        {...this.props}
+        incrementIfOdd={this.incrementIfOdd}
+        incrementAsync={this.incrementAsync}
+      />
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    count: state
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { increment, decrement }
+)(CounterContainer);
