4cde7d1d9b91f8b1db5072ed387cce9e661cfc75
jordan-massingill <jordan_massingill@hotmail.com>
2018-08-13 18:07:27 -0500

    Added styling
    Styled button page
    Used reactstrap and styled-components

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,40 +1,110 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
 import { increment, decrement } from '../actions';
+import { Button, Row, Col } from 'reactstrap';
+import styled from 'styled-components';
+
+const P = styled(Col) `
+  font-weight: bold;
+  text-align: center;
+  font-size: 50px;
+  color: white;
+  background: #031424
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 300px;
+`;
+
+const TopBtnDiv = styled(Col) `
+  display: flex;
+  flex-direction: column;
+`;
+
+const S1Button = styled(Button) `
+  background: #30415D;
+  color: #D9B310;
+  width: 190px;
+  height: 100px;
+  font-size: 40px
+  margin-bottom: 10px;
+`;
+
+const S2Button = styled(Button) `
+  background: #30415D;
+  color: #D9B310;
+  font-weight: bold;
+  width: 300px;
+  height: 100px;
+  font-size: 20px
+  margin-bottom: 10px;
+`;
+
+const MainDiv = styled.div `
+  margin: 0 auto;
+  max-width: 950px;
+  width: 100%;
+  flex-direction: column;
+  background-image: url("https://i.pinimg.com/736x/d1/92/4c/d1924c9919488b6057646eabf0832b57--screen-wallpaper-backgrounds-free.jpg");
+  background-size: cover;
+  padding: 50px 30px;
+`;
+
+const TopDiv = styled(Row) `
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  margin-bottom: 200px;
+  margin-bottom: 50px;
+`;
+
+const BottomDiv = styled(Row) `
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 200px;
+`;
 
 class Counter extends Component {
     incrementIfOdd = () => {
-        // Stretch Problem: Implement an increment function that
-        // only increments if the counter value is odd
+      if (this.props.count % 2 !== 0)
+      {this.props.increment()}
     };
 
     incrementAsync = () => {
         // Stretch Problem: Implement an increment function that
         // increments after waiting for one second
-    };
+    setTimeout(this.props.increment, 1000)
+};
 
     render() {
         // Fill in the two button onClick methods
         // Upon clicking these buttons, the count
         // should decrement or increment accordingly
         return (
-            <p>
-                Clicked: {this.props.count} times
-                <button onClick={() => this.props.increment() }>
+          <MainDiv>
+            <TopDiv>
+              <P xs='6'>
+                Clicked: <br/>{this.props.count} times
+              </P>
+              <TopBtnDiv xs='3'>
+                <S1Button onClick={() => this.props.increment() }>
                     +
-                </button>
-                <button onClick={() => this.props.decrement() }>
+                </S1Button>
+                <S1Button onClick={() => this.props.decrement() }>
                     -
-                </button>
-                 {/* Uncomment these button tags if you got
-                around to implementing the extra credit functions */}
-                {/* <button onClick={this.incrementIfOdd}>
-                    Increment if odd
-                </button>
-                <button onClick={this.incrementAsync}>
-                    Increment async
-                </button>  */}
-            </p>
+                </S1Button> <br/>
+              </TopBtnDiv>
+            </TopDiv>
+            <BottomDiv>
+              <S2Button onClick={this.incrementIfOdd}>
+                  Increment if odd
+              </S2Button>
+              <S2Button onClick={this.incrementAsync}>
+                  Increment async
+              </S2Button>
+            </BottomDiv>
+          </MainDiv>
         );
     }
 }
