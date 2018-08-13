dcaddfd49e6e85aac5eed9cc08f6bbc142a22363
luis salazar <luissalazar@luiss-MacBook-Pro.local>
2018-08-13 13:25:53 -0700

    First PR

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -20,10 +20,10 @@ class Counter extends Component {
         return (
             <p>
                 Clicked: {this.props.count} times
-                <button onClick={() => /* Fill me in */ }>
+                <button >
                     +
                 </button>
-                <button onClick={() => /* Fill me in */ }>
+                <button >
                     -
                 </button>
                  {/* Uncomment these button tags if you got
