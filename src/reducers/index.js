2fd42ed14c152f32fb77b8576150ed57f7d1cca7
sliw <slioweng@gmail.com>
2018-08-14 05:24:01 +0800

    Steven L

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,16 +1,12 @@
 import { INCREMENT, DECREMENT } from '../actions';
+import { Object } from 'core-js';
 
-// Our reducer that handles our two action cases:
-// increment and decrement. It receives the state
-// of our redux store, along with an action created
-// by our action creator. What does the reducer
-// need to do with the count in each case?
 export default (count = 0, action) => {
   switch (action.type) {
     case INCREMENT:
-    // Fill in the body of this case
+      return count + 1;
     case DECREMENT:
-    // Fill in the body of this case
+      return count - 1;
     default:
       return count;
   }
