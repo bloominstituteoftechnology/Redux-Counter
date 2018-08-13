d559582325873b519d87edbcd86e12724a23aeda
francistse23 <wtse.ucsd@gmail.com>
2018-08-13 13:12:01 -0700

    counter working

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -8,13 +8,9 @@ import { INCREMENT, DECREMENT } from '../actions';
 export default (count = 0, action) => {
   switch (action.type) {
     case INCREMENT:
-      return Object.assign({}, state, {
-        count: state.count +1
-      });
+      return count+1;
     case DECREMENT:
-    return Object.assign({}, state,{
-      count: state.count -1
-    })
+      return count-1;
     default:
       return count;
   }
