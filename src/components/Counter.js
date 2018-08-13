f64b72b41b71dd12bf57e8ad561378688611ab0f
dasgma <dasgrigoma@gmail.com>
2018-08-13 12:44:40 -0700

    SUrprisingly finished main tasks

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -20,10 +20,10 @@ class Counter extends Component {
         return (
             <p>
                 Clicked: {this.props.count} times
-                <button onClick={() => /* Fill me in */ }>
+                <button onClick={() => this.props.increment(this.count)}>
                     +
                 </button>
-                <button onClick={() => /* Fill me in */ }>
+                <button onClick={() => this.props.decrement(this.count) }>
                     -
                 </button>
                  {/* Uncomment these button tags if you got
@@ -33,7 +33,7 @@ class Counter extends Component {
                 </button>
                 <button onClick={this.incrementAsync}>
                     Increment async
-                </button>  */}
+                </button>  */} 
             </p>
         );
     }
