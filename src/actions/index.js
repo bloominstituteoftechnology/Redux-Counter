a4adea57f9debb50ac28a2ca20cb6e9fbaf747ca
Alberto-Funk <laptopreneur@gmail.com>
2018-08-14 23:13:06 -0700

    MVP Complete

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,8 @@
+
+// Source of truth for the types of action
 export const INCREMENT = 'INCREMENT';
 export const DECREMENT = 'DECREMENT';
+export const INCREMENTIFODD = 'INCREMENTIFODD';
 
 // Our action creators will return
 // an action packet that our reducer will
@@ -9,10 +12,27 @@ export const DECREMENT = 'DECREMENT';
 // logic of updating the central Redux store. That
 // is left to the reducer(s).
 
+// You do not need to import these in the reducer files, redux automatically connects these actions to the reducer, and sends it the action(type/payload) that you return
+
 export const increment = () => {
   // Fill in this function
+  return {
+    type: INCREMENT,
+    payload: 1
+  }
 };
 
-export const decrement = () => {
+// you can do it either or
+export const decrement = () => ({
   // Fill in this function
+    type: DECREMENT,
+    payload: 1
+});
+
+export const incrementIfOdd = () => {
+  // Fill in this function
+  return {
+    type: INCREMENTIFODD,
+    payload: 1
+  }
 };
