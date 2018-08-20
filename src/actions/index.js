9a2aec16fcf44ae0a5c23de64711d67cec13a35a
AlexanderCaves <ahcaves@gmail.com>
2018-08-20 16:25:21 -0700

    project complete

--- src/actions/index.js
+++ src/actions/index.js
@@ -11,9 +11,14 @@ export const DECREMENT = 'DECREMENT';
 
 export const increment = () => {
   // Fill in this function
+return {
+  type: INCREMENT
+}
 };
 
 export const decrement = () => {
   // Fill in this function
+  return {
+    type: DECREMENT
+  }
 };
-start
