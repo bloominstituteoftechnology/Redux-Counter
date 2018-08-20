958b9fd630e887b3d1102f91602b76c12cde49a2
bri79319 <bri79319@gmail.com>
2018-08-20 19:13:33 -0400

    changes to code

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -14,10 +14,12 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
     return {
+      ...state,
       count: state.count + 1,
     }
     case DECREMENT:
     return {
+      ...state,
       count: state.count - 1,
     }
     default:
