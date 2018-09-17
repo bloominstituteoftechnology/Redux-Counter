72f23465d8b53a63c3d86d9d7f3fbd7ebc0a9b40
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-09-17 16:58:57 -0400

    Stretch complete Updated counter component to implement a button that has the function to increment the counter value if its odd

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -6,6 +6,9 @@ class Counter extends Component {
     incrementIfOdd = () => {
         // Stretch Problem: Implement an increment function that
         // only increments if the counter value is odd
+        if (this.props.count%2!==0) {
+            this.props.increment();
+        }
     };
     
     
@@ -42,9 +45,9 @@ class Counter extends Component {
                 </button>
                  {/* Uncomment these button tags if you got
                 around to implementing the extra credit functions */}
-                {/* <button onClick={this.incrementIfOdd}>
+                 <button onClick={this.incrementIfOdd}>
                     Increment if odd
-            </button>*/}
+            </button>
                 <button onClick={this.incrementAsync}>
                     Increment async
                 </button>  
