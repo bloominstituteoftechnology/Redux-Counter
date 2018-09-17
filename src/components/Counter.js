95d7f7d6fa35aaf9421d6b955d4d68049f84bde4
lndubose <lndubose13@gmail.com>
2018-09-17 15:26:51 -0500

    increment async working

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -11,6 +11,9 @@ class Counter extends Component {
     incrementAsync = () => {
         // Stretch Problem: Implement an increment function that
         // increments after waiting for one second
+        setTimeout(() => {
+            this.props.increment()
+        }, 1000)
     };
 
     render() {
@@ -28,12 +31,12 @@ class Counter extends Component {
                 </button>
                  {/* Uncomment these button tags if you got
                 around to implementing the extra credit functions */}
-                {/* <button onClick={this.incrementIfOdd}>
+                <button onClick={this.incrementIfOdd}>
                     Increment if odd
                 </button>
                 <button onClick={this.incrementAsync}>
                     Increment async
-                </button>  */}
+                </button> 
             </p>
         );
     }
