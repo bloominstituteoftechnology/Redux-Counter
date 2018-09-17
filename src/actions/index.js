124d26af7e5f348a81322e090dd3d662ad30aa12
Joseph Bradley <josephbradley@Josephs-MacBook-Air.local>
2018-09-17 14:21:42 -0700

    displaying app...moving into inc/dec functionality

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,10 +9,12 @@ export const DECREMENT = 'DECREMENT';
 // logic of updating the central Redux store. That
 // is left to the reducer(s).
 
-export const increment = () => {
+export const increment = counter => {
   // Fill in this function
+  return { type: 'INCREMENT', payload: counter}
 };
 
-export const decrement = () => {
+export const decrement = counter => {
   // Fill in this function
+  return { type: 'DECREMENT', payload: counter}
 };
