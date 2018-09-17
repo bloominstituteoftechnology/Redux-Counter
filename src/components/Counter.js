fc76c722cdf06ec16dafafd091d88e498da8504c
Vera Simon <ghjkl>
2018-09-17 15:20:25 -0700

    Redux Counter
    Got MVP working with a bit of help from Braden

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -20,10 +20,10 @@ class Counter extends Component {
         return (
             <p>
                 Clicked: {this.props.count} times
-                <button onClick={() => /* Fill me in */ }>
+                <button onClick={() => /* Fill me in */this.props.increment(this.props.count) }>
                     +
                 </button>
-                <button onClick={() => /* Fill me in */ }>
+                <button onClick={() => /* Fill me in */this.props.decrement(this.props.count) }>
                     -
                 </button>
                  {/* Uncomment these button tags if you got
