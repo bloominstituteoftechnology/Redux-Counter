2c64e0564ae6991f10622a2e79ed9ee341afa32b
Michael Agard <mdagard@gmail.com>
2018-09-17 17:34:32 -0400

    Removed tons of unneccessary words. Wasted tons of time here.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,5 @@
 export const INCREMENT = 'INCREMENT';
 export const DECREMENT = 'DECREMENT';
-
 // Our action creators will return
 // an action packet that our reducer will
 // receive. What does the action packet look like?
@@ -9,22 +8,12 @@ export const DECREMENT = 'DECREMENT';
 // logic of updating the central Redux store. That
 // is left to the reducer(s).
 
-export const increment = (state = initialState, action) => {
-  console.log('in reducer: ', action);
-  switch (action.type) {
-    case 'CHANGE_NUMBER':
-      return { ...state, number: action.payload };
-    default:
-      return state;
-  }
+export const increment = num => {
+  console.log('in action: ', num);
+  return { type: 'INCREMENT' };
 };
 
-export const decrement = (state = initialState, action) => {
-  console.log('in reducer: ', action);
-  switch (action.type) {
-    case 'CHANGE_NUMBER':
-      return { ...state, number: action.payload };
-    default:
-      return state;
-  }
+export const decrement = num => {
+  console.log('in action: ', num);
+  return { type: 'DECREMENT' };
 };
