8c7d727821eac7b768986bc28c6d2ed75df72c2b
Kevin Sooter <kevin.m.sooter@gmail.com>
2018-09-17 15:30:15 -0500

    Added increment and decrement feature

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,12 +5,15 @@ import { INCREMENT, DECREMENT } from '../actions';
 // of our redux store, along with an action created
 // by our action creator. What does the reducer
 // need to do with the count in each case?
+
 export default (count = 0, action) => {
   switch (action.type) {
     case INCREMENT:
-    // Fill in the body of this case
+      return count + 1;
+
     case DECREMENT:
-    // Fill in the body of this case
+      return count - 1;
+
     default:
       return count;
   }
