6e9e4a2d034eecf2736eb40844159245e6d803f2
lndubose <lndubose13@gmail.com>
2018-09-17 15:41:35 -0500

    prettied

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
-export const INCREMENT = 'INCREMENT';
-export const DECREMENT = 'DECREMENT';
+export const INCREMENT = "INCREMENT";
+export const DECREMENT = "DECREMENT";
 
 // Our action creators will return
 // an action packet that our reducer will
@@ -11,12 +11,10 @@ export const DECREMENT = 'DECREMENT';
 
 export const increment = () => {
   // Fill in this function
-  console.log('action running')
-  return { type: INCREMENT }
-
+  return { type: INCREMENT };
 };
 
 export const decrement = () => {
   // Fill in this function
-  return { type: DECREMENT }
+  return { type: DECREMENT };
 };
