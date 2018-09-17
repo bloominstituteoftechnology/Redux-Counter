4113f87f5b8f7765ee60a2e84f9a74d2001ef498
David Benavidez <benavidez_david13@yahoo.com>
2018-09-17 15:18:09 -0700

    finshed assignemnt

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -9,8 +9,10 @@ export default (count = 0, action) => {
   switch (action.type) {
     case INCREMENT:
     // Fill in the body of this case
+    return action.payload + 1;
     case DECREMENT:
     // Fill in the body of this case
+    return action.payload - 1;
     default:
       return count;
   }
