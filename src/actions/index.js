de73c337c553e99aada31d5e7d304a26d6d2da5a
Brandon White <bwhite06@stac.edu>
2018-09-24 15:19:13 -0400

    intial commit

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 export const INCREMENT = 'INCREMENT';
 export const DECREMENT = 'DECREMENT';
 
-// Our action creators will return
+// Our action creators will return/
 // an action packet that our reducer will
 // receive. What does the action packet look like?
 // Note that the action creator is not at all
