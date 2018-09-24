985eb5a809067de2e1780f598f7eb1b3bd0d650d
IlanL75 <anatoly.leytman@gmail.com>
2018-09-24 15:16:32 -0700

    Adding incrementIfOdd & incrementAsync

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -6,11 +6,17 @@ class Counter extends Component {
     incrementIfOdd = () => {
         // Stretch Problem: Implement an increment function that
         // only increments if the counter value is odd
+        if(this.props.count % 2 !== 0) {
+            this.props.increment();
+        }
     };
 
     incrementAsync = () => {
         // Stretch Problem: Implement an increment function that
         // increments after waiting for one second
+        setTimeout(() => {
+            this.props.increment();
+        }, 1000);
     };
 
     render() {
@@ -25,6 +31,12 @@ class Counter extends Component {
                 </button>
                 <button onClick={() => this.props.decrement()/* Fill me in */ }>
                     -
+                </button>
+                <button onClick={this.incrementIfOdd}>
+                    + if odd
+                </button>
+                <button onClick={this.incrementAsync}>
+                    + after second
                 </button>
                  {/* Uncomment these button tags if you got
                 around to implementing the extra credit functions */}
