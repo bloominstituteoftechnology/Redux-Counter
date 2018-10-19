21674806023d3087e1f2d67976633707b1afc924
Lee Len Formento <leebatef@gmail.com>
2018-10-19 15:01:21 -0400

    Filled in action functions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 export const INCREMENT = 'INCREMENT';
 export const DECREMENT = 'DECREMENT';
 
+//ACTIONS
 // Our action creators will return
 // an action packet that our reducer will
 // receive. What does the action packet look like?
@@ -10,9 +11,17 @@ export const DECREMENT = 'DECREMENT';
 // is left to the reducer(s).
 
 export const increment = () => {
-  // Fill in this function
+  // Fill in this function 
+  return {
+  type: 'INCREMENT_NUM',
+  payload: 1
+  }
 };
 
 export const decrement = () => {
   // Fill in this function
+  return {
+  type: 'DECREMENT_NUM',
+  payload: 1
+  }
 };
