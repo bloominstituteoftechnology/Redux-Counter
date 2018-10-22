aa0cedced06ee83941cf95d8b3371f29a501cc77
BrandonVilaychith <blvilaychith@gmail.com>
2018-10-22 17:54:18 -0500

    format fixed

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -41,7 +41,7 @@ class Counter extends Component {
 // this component receives the whole state. In a more complex
 // redux application, though, it would receive only the relevant
 // parts it needs from the state object.
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     count: state.count
   };
@@ -52,7 +52,4 @@ const mapStateToProps = state => {
 // is only a dumb React component. We pass in all of the functions that
 // are reliant on Redux, along with the component itself, so that Redux
 // makes itself known to this component.
-export default connect(
-  mapStateToProps,
-  { increment, decrement }
-)(Counter);
+export default connect(mapStateToProps,{ increment, decrement })(Counter);
