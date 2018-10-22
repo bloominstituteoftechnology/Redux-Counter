f073a4a31355c27ef3b0544c11ca6d88ad7119f9
kyranmccann <kyran.mccann@gmail.com>
2018-10-22 11:54:05 -0700

    did not need to pass count to action

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,14 +9,14 @@ export const DECREMENT = 'DECREMENT';
 // logic of updating the central Redux store. That
 // is left to the reducer(s).
 
-export const increment = count => {
+export const increment = () => {
   return {
     type: INCREMENT,
 
   }
 };
 
-export const decrement = count => {
+export const decrement = () => {
   return {
     type: DECREMENT,
 
