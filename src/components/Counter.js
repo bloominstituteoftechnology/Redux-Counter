306a6f5373e84aeff61370ac599184f6a9ada871
KHollobaugh <khollobaugh@gmail.com>
2018-10-22 16:07:23 -0400

    added action creators

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -20,10 +20,10 @@ class Counter extends Component {
         return (
             <p>
                 Clicked: {this.props.count} times
-                <button onClick={(state) => increment(state.count) }>
+                <button onClick={() => this.props.increment() }>
                     +
                 </button>
-                <button onClick={(state) => decrement(state.count) }>
+                <button onClick={() => this.props.decrement() }>
                     -
                 </button>
                  {/* Uncomment these button tags if you got
