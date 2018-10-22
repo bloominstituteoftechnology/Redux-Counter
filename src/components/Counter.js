3f6b36e1cab5daf2b1b1a92f7a4e0b9b814acd50
KHollobaugh <khollobaugh@gmail.com>
2018-10-22 16:00:24 -0400

    initial commit, resolving errors in counter.js

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -20,10 +20,10 @@ class Counter extends Component {
         return (
             <p>
                 Clicked: {this.props.count} times
-                <button onClick={() => /* Fill me in */ }>
+                <button onClick={(state) => increment(state.count) }>
                     +
                 </button>
-                <button onClick={() => /* Fill me in */ }>
+                <button onClick={(state) => decrement(state.count) }>
                     -
                 </button>
                  {/* Uncomment these button tags if you got
