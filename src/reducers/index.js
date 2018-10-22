c9d83f89890cc202ca5f273634e3b259e874145d
lola <lev@lolas-air.lan>
2018-10-22 15:54:32 -0400

    Counter MVP functional

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -12,9 +12,9 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
-    // Fill in the body of this case
+      return { ...state, count: initialState.count++}
     case DECREMENT:
-    // Fill in the body of this case
+    return { ...state, count: initialState.count--}
     default:
       return state;
   }
