aea91f26aa215ed4214df63af6d76821b40e8940
Kat Fries <katherynejf@gmail.com>
2018-10-22 16:54:07 -0400

    completed counter without stretch

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,17 @@
 export const INCREMENT = 'INCREMENT';
 export const DECREMENT = 'DECREMENT';
 
-// Our action creators will return
-// an action packet that our reducer will
-// receive. What does the action packet look like?
-// Note that the action creator is not at all
-// responsible for handling any of the actual
-// logic of updating the central Redux store. That
-// is left to the reducer(s).
 
 export const increment = () => {
-  // Fill in this function
+  return {
+    type: INCREMENT,
+    payload: 1
+  };
 };
 
 export const decrement = () => {
-  // Fill in this function
+  return {
+    type: DECREMENT,
+    payload: 1
+  };
 };
