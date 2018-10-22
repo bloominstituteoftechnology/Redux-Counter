3462ec9d7bc35809d160f45f34c45396f390cc33
Carlos Lantigua <clantigua954@gmail.com>
2018-10-22 14:55:38 -0400

    got the buttons to work, still confused by all of this

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,54 +1,50 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { increment, decrement } from '../actions';
 
 class Counter extends Component {
-    incrementIfOdd = () => {
-        // Stretch Problem: Implement an increment function that
-        // only increments if the counter value is odd
-    };
+	incrementIfOdd = () => {
+		// Stretch Problem: Implement an increment function that
+		// only increments if the counter value is odd
+	};
 
-    incrementAsync = () => {
-        // Stretch Problem: Implement an increment function that
-        // increments after waiting for one second
-    };
+	incrementAsync = () => {
+		// Stretch Problem: Implement an increment function that
+		// increments after waiting for one second
+	};
 
-    render() {
-        // Fill in the two button onClick methods
-        // Upon clicking these buttons, the count
-        // should decrement or increment accordingly
-        return (
-            <p>
-                Clicked: {this.props.count} times
-                <button onClick={() => /* Fill me in */ }>
-                    +
-                </button>
-                <button onClick={() => /* Fill me in */ }>
-                    -
-                </button>
-                 {/* Uncomment these button tags if you got
+	render() {
+		// Fill in the two button onClick methods
+		// Upon clicking these buttons, the count
+		// should decrement or increment accordingly
+		return (
+			<p>
+				Clicked: {this.props.count} times
+				<button onClick={(count) => this.props.increment(this.state++)}>+</button>
+				<button onClick={(count) => this.props.decrement(this.state--)}>-</button>
+				{/* Uncomment these button tags if you got
                 around to implementing the extra credit functions */}
-                {/* <button onClick={this.incrementIfOdd}>
+				{/* <button onClick={this.incrementIfOdd}>
                     Increment if odd
                 </button>
                 <button onClick={this.incrementAsync}>
                     Increment async
                 </button>  */}
-            </p>
-        );
-    }
+			</p>
+		);
+	}
 }
 
-// The mapStateToProps function specifies which portion of the 
-// state tree this component needs to receive. In this case, 
+// The mapStateToProps function specifies which portion of the
+// state tree this component needs to receive. In this case,
 // since our redux store is only storing the value of the count,
 // this component receives the whole state. In a more complex
 // redux application, though, it would receive only the relevant
 // parts it needs from the state object.
 const mapStateToProps = (state) => {
-    return {
-        count: state.count
-    };
+	return {
+		count: state.count
+	};
 };
 
 // The connect function is called in order to make this component aware
