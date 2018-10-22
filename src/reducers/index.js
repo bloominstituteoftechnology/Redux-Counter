8b1a88b69e88e9e31a0aa7fe3baf322704147f1c
Unknown <zachary.arney@gmail.com>
2018-10-22 17:08:20 -0500

    Reducers update state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -12,10 +12,12 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
-    // Fill in the body of this case
+      // Fill in the body of this case
+      return { ...state, count: state.count + 1 };
     case DECREMENT:
-    // Fill in the body of this case
+      // Fill in the body of this case
+      return { ...state, count: state.count - 1 };
     default:
-      return count;
+      return state;
   }
 };
