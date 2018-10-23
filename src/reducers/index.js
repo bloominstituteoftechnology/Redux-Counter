4b6f06fdded5462164c2712ec504d935f798051b
bummings <eabummings@gmail.com>
2018-10-23 18:20:47 -0400

    finally got the increment/decrement functionality working

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,23 +1,27 @@
-import { INCREMENT, DECREMENT } from "../actions";
+// Reducers are pure functions, i.e. they produce no side
+// effects- they will return the same output given a fixed
+// input every single time.
+// You do not process sideeffect-ridden operations with a reducer, i.e.
+// fetching from an API, Math.random functionality, etc.
 
-const initialState = {
-  count: 0
-};
+import { INCREMENT, DECREMENT } from "../actions";
 
 // Our reducer that handles our two action cases:
 // increment and decrement. It receives the state
 // of our redux store, along with an action created
 // by our action creator. What does the reducer
 // need to do with the count in each case?
-export default (state = initialState, action) => {
+export default (count = 0, action) => {
   switch (action.type) {
     case INCREMENT:
       // Fill in the body of this case
-      count--;
+      return (count = count + action.payload); // AKA count += action.payload
+
     case DECREMENT:
       // Fill in the body of this case
-      count++;
+      return (count = count - action.payload);
+
     default:
-      return state;
+      return count;
   }
 };
