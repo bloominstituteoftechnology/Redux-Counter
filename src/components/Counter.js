37ce74c77253d4c1f24bbfe92312493066be8448
mikaelacurrier <mikaela.currier@gmail.com>
2018-10-29 18:46:51 -0400

    counter buttons added

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -20,12 +20,15 @@ class Counter extends Component {
         return (
             <p>
                 Clicked: {this.props.count} times
-                <button onClick={() => /* Fill me in */ }>
+                {' '}
+                <button onClick={() => this.props.increment() }>
                     +
                 </button>
-                <button onClick={() => /* Fill me in */ }>
+                {' '}
+                <button onClick={() => this.props.decrement() }>
                     -
                 </button>
+                {' '}
                  {/* Uncomment these button tags if you got
                 around to implementing the extra credit functions */}
                 {/* <button onClick={this.incrementIfOdd}>
