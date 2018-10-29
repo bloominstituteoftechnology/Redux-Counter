d7142f70db187781b8b5996fd3b8be8f0d91bb90
mm2079 <vayne3252@gmail.com>
2018-10-29 15:55:50 -0400

    Useless commit

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ const store = createStore(counter);
 // This is needed because the store needs to know where it
 // is passing its state to. The Provider component is also
 // where the store "lives".
+//
 ReactDOM.render(
   <Provider store={store}>
     <Counter />
