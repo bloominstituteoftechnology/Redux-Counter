fbda22acb81329920bfb70d5a729cca52b20a4aa
Jake Thomas <jmaxt12@gmail.com>
2018-10-29 14:26:15 -0600

    Redux counter complete

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,9 +10,17 @@ export const DECREMENT = 'DECREMENT';
 // is left to the reducer(s).
 
 export const increment = () => {
+  return {
+    type: INCREMENT
+   
+  };
   // Fill in this function
 };
 
 export const decrement = () => {
+  return {
+    type: DECREMENT
+ 
+  };
   // Fill in this function
 };
