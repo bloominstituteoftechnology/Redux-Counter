9ccdd0561c337d5d46b88f105d68b246631feb78
alasalle <angela.lasalle91@gmail.com>
2018-10-29 20:52:06 -0400

    completed MVP completed increment button functionality completed decrement button functionality

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -13,8 +13,16 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
     // Fill in the body of this case
+    return {...state, count: ++state.count};
+    
     case DECREMENT:
     // Fill in the body of this case
+    if(state.count === 0) {
+      return {...state, count: state.count}
+    } else {
+      return {...state, count: --state.count}
+    }
+    
     default:
       return state;
   }
