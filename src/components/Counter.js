d32847ed94dd689c1fe275b36b32c94fb169f679
Eric Fleming <efleming111@yahoo.com>
2018-10-30 09:52:24 -0700

    Added increment odd and increment async to counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,14 +3,19 @@ import { connect } from 'react-redux';
 import { increment, decrement } from '../actions';
 
 class Counter extends Component {
-    incrementIfOdd = () => {
+    incrementIfOdd = (event) => {
         // Stretch Problem: Implement an increment function that
         // only increments if the counter value is odd
+        event.preventDefault();
+        if(this.props.count % 2 !== 0){
+            this.props.increment();
+        }
     };
 
     incrementAsync = () => {
         // Stretch Problem: Implement an increment function that
         // increments after waiting for one second
+        setTimeout(()=>this.props.increment(), 1000);
     };
 
     render() {
@@ -28,12 +33,12 @@ class Counter extends Component {
                 </button>
                  {/* Uncomment these button tags if you got
                 around to implementing the extra credit functions */}
-                {/* <button onClick={this.incrementIfOdd}>
+                <button onClick={this.incrementIfOdd}>
                     Increment if odd
                 </button>
                 <button onClick={this.incrementAsync}>
                     Increment async
-                </button>  */}
+                </button> 
             </p>
         );
     }
