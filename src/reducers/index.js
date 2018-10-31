02084e45bb362d7e940803e16ebe84bd7af55cc0
iepoch <staclem@live.com>
2018-10-30 20:31:04 -0700

    Added the increment correctly,  updated buttons to the correct call to the increment.  Added dispatch type

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -12,10 +12,9 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
-      return state.count + 1
-
+      return {count: state.count + 1 }
     case DECREMENT:
-      return state.count - 1
+     return  {count: state.count - 1 }
     default:
       return state;
   }
