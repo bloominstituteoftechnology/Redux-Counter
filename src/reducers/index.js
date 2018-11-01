1620694ffb6c094b33c2baf4a0225c85fcd60f13
Ryntak94 <ryan.matthews.professional@gmail.com>
2018-10-31 21:41:24 -0400

    Fix switch statements

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -12,9 +12,11 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
-    // Fill in the body of this case
+        return { count: state.count + 1}
+        break;
     case DECREMENT:
-    // Fill in the body of this case
+        return { count: state.count - 1}
+        break;
     default:
       return state;
   }
