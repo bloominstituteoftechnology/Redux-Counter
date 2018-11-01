a3978a6073a004cd0c4980114dc5a4af5c9bc982
Joseph Thompson <joe.thompson838@gmail.com>
2018-10-31 20:40:56 -0400

    added code to actions, reducers, and counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -20,10 +20,10 @@ class Counter extends Component {
         return (
             <p>
                 Clicked: {this.props.count} times
-                <button onClick={() => /* Fill me in */ }>
+                <button onClick={() => /* Fill me in */ this.props.increment() }>
                     +
                 </button>
-                <button onClick={() => /* Fill me in */ }>
+                <button onClick={() => /* Fill me in */ this.props.decrement() }>
                     -
                 </button>
                  {/* Uncomment these button tags if you got
@@ -39,15 +39,15 @@ class Counter extends Component {
     }
 }
 
-// The mapStateToProps function specifies which portion of the 
-// state tree this component needs to receive. In this case, 
+// The mapStateToProps function specifies which portion of the
+// state tree this component needs to receive. In this case,
 // since our redux store is only storing the value of the count,
 // this component receives the whole state. In a more complex
 // redux application, though, it would receive only the relevant
 // parts it needs from the state object.
 const mapStateToProps = (state) => {
     return {
-        count: state.count
+        count: state
     };
 };
 
--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import counter from './reducers';
 // that is responsible for updating the store, along
 // with any initial state that we may want the store
 // to start out with (which is none in this case).
+
 const store = createStore(counter);
 
 // Here, we wrap our main React component inside of
