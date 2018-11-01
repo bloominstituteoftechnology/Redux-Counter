e1ad4841f4bbbd3c7c0ad03843ef5b24d86fe3a5
rlmclaughlin <rlmclaughlin@yahoo.com>
2018-10-31 22:29:50 -0500

    finished second stretch question

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -10,8 +10,7 @@ class Counter extends Component {
     };
 
     incrementAsync = () => {
-        // Stretch Problem: Implement an increment function that
-        // increments after waiting for one second
+        setTimeout(() => this.props.increment(),1000) 
     };
 
     render() {
