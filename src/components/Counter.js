e5cbcc5d2596f1b9a1f117a37ec181ec7011793f
Witanday <witanday.cd@gmail.com>
2018-11-01 23:39:48 -0400

    STRETCH PROBLEM COPMLETED

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,14 +5,16 @@ import { increment, decrement } from '../actions';
 class Counter extends Component {
    
     
-    incrementIfOdd = () => {
+    incrementIfOdd = (n) => {
         // Stretch Problem: Implement an increment function that
         // only increments if the counter value is odd
+       if (parseInt(n)  % 2 === 0 ){return this.props.increment} 
     };
 
     incrementAsync = () => {
         // Stretch Problem: Implement an increment function that
         // increments after waiting for one second
+        setTimeout(this.props.increment, 1000)
     };
 
     render() {
@@ -28,14 +30,13 @@ class Counter extends Component {
                 <button onClick={/* Fill me in */ this.props.decrement }>
                     -
                 </button>
-                 {/* Uncomment these button tags if you got
-                around to implementing the extra credit functions */}
-                {/* <button onClick={this.incrementIfOdd}>
+                  
+                <button onClick={this.incrementIfOdd (this.props.count)}>
                     Increment if odd
                 </button>
                 <button onClick={this.incrementAsync}>
                     Increment async
-                </button>  */}
+                </button>  
             </p>
         );
     }
