abf08b5abba81550899d0e7aa8868ae2a2a7add8
Jesse <theiseja@gmail.com>
2018-11-06 22:27:19 -0600

    Got the counter to display

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -12,9 +12,9 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
-    // Fill in the body of this case
+        return ++state;
     case DECREMENT:
-    // Fill in the body of this case
+        return --state;
     default:
       return state;
   }
