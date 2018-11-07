87df311b83d298d69de3b8aae78a5ef8504f40a0
cocoitali <cmburatto@gmail.com>
2018-11-07 14:14:35 -0800

    MVP COMPLETE (made changes to reducers)

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -12,10 +12,10 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
-      return state + 1;
+      return {count: state.count + 1}
 
     case DECREMENT:
-      return state - 1;
+      return {count:state.count - 1}
     default:
       return state;
   }
