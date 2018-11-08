42e3ae00494ce70b0354dc0920c3c5e8af769b77
Victor G <victorgordian103@gmail.com>
2018-11-07 21:21:01 -0800

    Actions and reducers

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -20,10 +20,10 @@ class Counter extends Component {
         return (
             <p>
                 Clicked: {this.props.count} times
-                <button onClick={() => {return ++this.props.count }>
+                <button onClick={() => {return ++this.props.count }}>
                     +
                 </button>
-                <button onClick={() => /* Fill me in */ }>
+                <button onClick={() => {return --this.props.count} }>
                     -
                 </button>
                  {/* Uncomment these button tags if you got
