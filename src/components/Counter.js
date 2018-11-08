8a6311f19889983f3d6f78b0eb8f5e323e6813eb
Jesse <theiseja@gmail.com>
2018-11-07 22:44:42 -0600

    Fixed the favicon issue by deleting it

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import { increment, decrement } from '../actions';
 
 class Counter extends Component {
+        // Stretch Problem: Implement an increment function that
+        // only increments if the counter value is odd
     incrementIfOdd = () => {
         if (this.props.count % 2 !== 0) {
             this.props.increment();
