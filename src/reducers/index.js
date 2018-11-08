76d7447e93102dad57767adc4fb86a34d4d95295
erscriber <erscriber@gmail.com>
2018-11-07 23:21:54 -0500

    Updated increment/decrement functions in action/index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -13,8 +13,10 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
     // Fill in the body of this case
+    
     case DECREMENT:
     // Fill in the body of this case
+    
     default:
       return state;
   }
