3da5a73a29784f278422307884d25083b72535fa
cocoitali <cmburatto@gmail.com>
2018-11-12 12:51:18 -0800

    STRETCH (implemented async func)

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,32 +4,25 @@ import { increment, decrement } from "../actions";
 
 class Counter extends Component {
   incrementIfOdd = () => {
+
     // Stretch Problem: Implement an increment function that
     // only increments if the counter value is odd
   };
 
   incrementAsync = () => {
+    setTimeout(()=> this.props.increment(), 1000)
     // Stretch Problem: Implement an increment function that
     // increments after waiting for one second
   };
 
   render() {
-    // Fill in the two button onClick methods
-    // Upon clicking these buttons, the count
-    // should decrement or increment accordingly
     return (
       <p>
         Clicked: {this.props.count} times
         <button onClick={() => this.props.increment()}>+</button>
         <button onClick={() => this.props.decrement()}>-</button>
-        {/* Uncomment these button tags if you got
-                around to implementing the extra credit functions */}
-        {/* <button onClick={this.incrementIfOdd}>
-                    Increment if odd
-                </button>
-                <button onClick={this.incrementAsync}>
-                    Increment async
-                </button>  */}
+        <button onClick={this.incrementIfOdd}>Increment if odd</button>
+        <button onClick={this.incrementAsync}>Increment async</button>
       </p>
     );
   }
@@ -42,6 +35,7 @@ class Counter extends Component {
 // redux application, though, it would receive only the relevant
 // parts it needs from the state object.
 const mapStateToProps = state => {
+  console.log("STATE", state);
   return {
     count: state.count
   };
