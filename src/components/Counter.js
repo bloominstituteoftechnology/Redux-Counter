588cbf8e87475e1710a5044b4ff09a44f4dc4549
Stephen Bondor <bondor@Stephens-MacBook-Pro.local>
2018-12-03 13:54:30 -0800

    Add all stretch goals

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -13,6 +13,10 @@ class Counter extends Component {
     incrementAsync = () => {
         // Stretch Problem: Implement an increment function that
         // increments after waiting for one second
+        setTimeout(() => {
+            this.props.increment()
+        }, 1000)
+        
     };
 
     render() {
@@ -22,7 +26,7 @@ class Counter extends Component {
         return (
             <p>
                 Clicked: {this.props.count} times
-                <button onClick={() => this.incrementIfOdd() }>
+                <button onClick={() => this.props.increment() }>
                     +
                 </button>
                 <button onClick={() => this.props.decrement() }>
@@ -30,12 +34,12 @@ class Counter extends Component {
                 </button>
                  {/* Uncomment these button tags if you got
                 around to implementing the extra credit functions */}
-                {/* <button onClick={this.incrementIfOdd}>
+                <button onClick={this.incrementIfOdd}>
                     Increment if odd
                 </button>
                 <button onClick={this.incrementAsync}>
                     Increment async
-                </button>  */}
+                </button> 
             </p>
         );
     }
