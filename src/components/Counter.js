a631b09e2f37450dfca81ba6a3b9ff1b56b105d2
dyiar <austin.hl.blake@gmail.com>
2018-12-03 14:57:00 -0700

    Fixed one small thing

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -6,7 +6,7 @@ class Counter extends Component {
   incrementIfOdd = () => {
     // Stretch Problem: Implement an increment function that
     // only increments if the counter value is odd
-    if (this.props.count % 2 != 0) {
+    if (this.props.count % 2 !== 0) {
       this.props.increment();
     }
   };
