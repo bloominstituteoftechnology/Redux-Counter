96284d5606701225f20c70ea828802484fe14cd9
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2018-12-03 16:37:53 -0600

    counter works. woot woot.

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -20,10 +20,10 @@ class Counter extends Component {
         return (
             <p>
                 Clicked: {this.props.count} times
-                <button onClick={() => increment() }>
+                <button onClick={() => this.props.increment()}>
                     +
                 </button>
-                <button onClick={() => decrement() }>
+                <button onClick={() => this.props.decrement()}>
                     -
                 </button>
                  {/* Uncomment these button tags if you got
@@ -50,6 +50,8 @@ const mapStateToProps = (state) => {
         count: state.count
     };
 };
+
+
 
 // The connect function is called in order to make this component aware
 // of the rest of the redux architecture. Without this, this component
