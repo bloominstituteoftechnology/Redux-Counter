2bc25358f8bc18f56a98010d6d93b2f1cd987c3f
steelworx <eileencuevas62@gmail.com>
2018-12-03 17:51:45 -0500

    completed stretch questions in Counter.js

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -8,11 +8,13 @@ class Counter extends Component {
     incrementIfOdd = () => {
         // Stretch Problem: Implement an increment function that
         // only increments if the counter value is odd
+        if (this.props.count % 2 === 1) this.props.increment(this.props.count);
     };
 
     incrementAsync = () => {
         // Stretch Problem: Implement an increment function that
         // increments after waiting for one second
+        setTimeout(() => this.props.increment(this.props.count), 1000);
     };
 
     render() {
@@ -25,19 +27,19 @@ class Counter extends Component {
                 <p className='count-number'>{this.props.count}</p>
                 <div className='button-container'>
                     <button className='increment-button' onClick={() =>  this.props.increment(this.props.count)}>
-                        +
+                        <i className="fas fa-plus"></i>
                     </button>
                     <button className='decrement-button' onClick={() => this.props.decrement(this.props.count)}>
-                        -
+                        <i className="fas fa-minus"></i>
                     </button>
                     {/* Uncomment these button tags if you got
                     around to implementing the extra credit functions */}
-                    {/* <button onClick={this.incrementIfOdd}>
+                    <button className='odd-button' onClick={this.incrementIfOdd}>
                         Increment if odd
                     </button>
-                    <button onClick={this.incrementAsync}>
+                    <button className='async-button' onClick={this.incrementAsync}>
                         Increment async
-                    </button>  */}
+                    </button> 
                 </div>
             </div>
         );
