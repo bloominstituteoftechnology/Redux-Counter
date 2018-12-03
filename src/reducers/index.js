0011f887e07ba2ae507bda0836f99b7df2b0969a
Lidii <lidiiany92@gmail.com>
2018-12-03 16:21:56 -0500

    Edited reducer and action index.js files

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -13,8 +13,18 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
     // Fill in the body of this case
+    return {
+      ...state, 
+      count: state.count++
+    };
+    
     case DECREMENT:
     // Fill in the body of this case
+    return {
+      ...state,
+      count: state.count--
+    };
+    
     default:
       return state;
   }
