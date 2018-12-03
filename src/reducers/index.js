ea17c1df5ae5196e271f6e4ef1fe3a065d64ab55
johnoro <officialjohnorourke@gmail.com>
2018-12-03 15:21:27 -0500

    Remove unnecessary comment

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,7 +4,6 @@ export default (counters = [{ count: 0, id: 0 }], action) => {
   const { type, payload = {} } = action;
   const { count, id = 0 } = payload;
   let i = counters.findIndex(c => c.id === id);
-  // console.log(id, i);
   let counts = counters.slice(0, i).concat(counters.slice(i + 1, counters.length));
   switch (type) {
     case CREATE:
