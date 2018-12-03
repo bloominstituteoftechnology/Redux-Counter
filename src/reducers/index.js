99e7c59c7b0c0dfc64f51d12a58a8c4a376b5989
brooks <bpoltl1@gmail.com>
2018-12-03 16:23:05 -0600

    mvp and stretch

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -11,9 +11,15 @@ const initialState = {
 // need to do with the count in each case?
 export default (state = initialState, action) => {
   switch (action.type) {
-    case INCREMENT:
+    case INCREMENT: return {
+      ...state,
+      count: state.count + 1
+    }
     // Fill in the body of this case
-    case DECREMENT:
+    case DECREMENT: return {
+      ...state,
+      count: state.count - 1
+    }
     // Fill in the body of this case
     default:
       return state;
