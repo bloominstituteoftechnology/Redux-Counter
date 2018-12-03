e8abe0dfa412d40b9d5b4dcff2e975a041d619a0
Dennis Orbison <orbison.dennis@gmail.com>
2018-12-03 15:46:02 -0800

    a commit

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -15,13 +15,13 @@ export default (state = initialState, action) => {
     // Fill in the body of this case
     return {
       ...state,
-      count: state.count++,
+      count: state.count + 1,
     }
     case DECREMENT:
     // Fill in the body of this case
     return {
       ...state,
-      count: state.count--,
+      count: state.count - 1,
     }
     default:
       return state;
