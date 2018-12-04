35c9f85ad437edcff914a13cc2d7e953dc0683fe
Holden B <holden.bucher.92@gmail.com>
2018-12-04 09:42:11 -0600

    cleandeded up muh codes

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -14,9 +14,6 @@ class Counter extends Component {
   };
 
   render() {
-    // Fill in the two button onClick methods
-    // Upon clicking these buttons, the count
-    // should decrement or increment accordingly
     return (
       <p>
         Clicked: {this.props.count} times
@@ -29,23 +26,12 @@ class Counter extends Component {
   }
 }
 
-// The mapStateToProps function specifies which portion of the
-// state tree this component needs to receive. In this case,
-// since our redux store is only storing the value of the count,
-// this component receives the whole state. In a more complex
-// redux application, though, it would receive only the relevant
-// parts it needs from the state object.
 const mapStateToProps = state => {
   return {
     count: state.count
   };
 };
 
-// The connect function is called in order to make this component aware
-// of the rest of the redux architecture. Without this, this component
-// is only a dumb React component. We pass in all of the functions that
-// are reliant on Redux, along with the component itself, so that Redux
-// makes itself known to this component.
 export default connect(
   mapStateToProps,
   { increment, decrement }
