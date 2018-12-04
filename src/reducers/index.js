8e0061c622f8fa21630b27ce653e8bfeafc36f33
Holden B <holden.bucher.92@gmail.com>
2018-12-04 09:35:48 -0600

    simplified logic for reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -14,12 +14,12 @@ export default (state = initialState, action) => {
     case INCREMENT:
       return {
         ...state,
-        count: (state.count += 1)
+        count: state.count + 1
       };
     case DECREMENT:
       return {
         ...state,
-        count: (state.count -= 1)
+        count: state.count - 1
       };
     default:
       return state;
