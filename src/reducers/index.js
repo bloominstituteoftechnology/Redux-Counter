e101e408cc769bcdb3ec85e939bac1e8994f9b07
ltann <leeroyleeroy18@gmail.com>
2019-01-27 18:05:56 -0800

    Finished MVP increment and decrement functionality

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -10,11 +10,14 @@ const initialState = {
 // by our action creator. What does the reducer
 // need to do with the count in each case?
 export default (state = initialState, action) => {
+  console.log('Inside reducer', state, action)
   switch (action.type) {
     case INCREMENT:
     // Fill in the body of this case
+      return { ...state, count: state.count + 1}
     case DECREMENT:
     // Fill in the body of this case
+      return { ...state, count: state.count - 1}
     default:
       return state;
   }
