de4af12978aa65baca76ac81ce27d968a657d1d6
Xavier Puentes <xavier@Xaviers-MacBook-Pro.local>
2019-01-30 00:21:32 -0600

    Removed optional payload property

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,10 +10,10 @@ export const DECREMENT = 'DECREMENT';
 // is left to the reducer(s).
 
 export const increment = () => {
-  return {type: INCREMENT, payload: 1 };
+  return {type: INCREMENT};
 };
 
 export const decrement = () => {
   // Fill in this function
-  return {type: DECREMENT, payload: 1};
+  return {type: DECREMENT};
 };
