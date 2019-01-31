29372981feab7aca20f5b84bcb4935c19c4f6353
JHaydenDev <34730927+JHaydenDev@users.noreply.github.com>
2019-01-30 21:43:17 -0600

    Stretch and MCP complete.

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,37 +3,27 @@ import { connect } from 'react-redux';
 import { increment, decrement } from '../actions';
 
 class Counter extends Component {
-    //incrementIfOdd = () => {
-        // Stretch Problem: Implement an increment function that
-        // only increments if the counter value is odd
-    //};
+    incrementIfOdd = () => {
+        if (this.props.count % 2 !== 0) {
+            this.props.increment();
+        }
+    };
 
-    //incrementAsync = () => {
-        // Stretch Problem: Implement an increment function that
-        // increments after waiting for one second
-    //};
+    incrementAsync = () => {
+        setTimeout(() => { this.props.increment(); }, 1000);
+    };
 
     render() {
-        // Fill in the two button onClick methods
-        // Upon clicking these buttons, the count
-        // should decrement or increment accordingly
         return (
             <p>
                 Clicked: {this.props.count} times
-                <button onClick={() => this.props.increment() }>
-                    +increment
-                </button>
-                <button onClick={() => this.props.decrement() }>
-                    -decrement
-                </button>
-                 {/* Uncomment these button tags if you got
-                around to implementing the extra credit functions */}
-                {/* <button onClick={this.incrementIfOdd}>
-                    Increment if odd
-                </button>
-                <button onClick={this.incrementAsync}>
-                    Increment async
-                </button>  */}
+                <button onClick={() => this.props.increment() }> +increment </button>
+
+                <button onClick={() => this.props.decrement() }> -decrement </button>
+
+                <button onClick={this.incrementIfOdd}> Increment if odd </button>
+
+                <button onClick={this.incrementAsync}> Increment async </button>
             </p>
         );
     }
