bb8b0be6a584a7e11875848aec27452cb43dd555
CatsAreMLG <joshuadgon@gmail.com>
2019-01-30 18:11:56 -0600

    Cleaned up code

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,22 +1,10 @@
 export const INCREMENT = "INCREMENT";
 export const DECREMENT = "DECREMENT";
 
-// Our action creators will return
-// an action packet that our reducer will
-// receive. What does the action packet look like?
-// Note that the action creator is not at all
-// responsible for handling any of the actual
-// logic of updating the central Redux store. That
-// is left to the reducer(s).
-
 export const increment = c => {
-  // Fill in this function
-  let counter = c + 1;
-  return { type: INCREMENT, payload: { count: counter } };
+  return { type: INCREMENT, count: c + 1 };
 };
 
 export const decrement = c => {
-  // Fill in this function
-  let counter = c - 1;
-  return { type: DECREMENT, payload: { count: counter } };
+  return { type: DECREMENT, count: c - 1 };
 };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,17 +4,12 @@ const initialState = {
   count: 0
 };
 
-// Our reducer that handles our two action cases:
-// increment and decrement. It receives the state
-// of our redux store, along with an action created
-// by our action creator. What does the reducer
-// need to do with the count in each case?
 export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
-      return { ...state, count: action.payload.count };
+      return { count: action.count };
     case DECREMENT:
-      return { ...state, count: action.payload.count };
+      return { count: action.count };
     default:
       return state;
   }
