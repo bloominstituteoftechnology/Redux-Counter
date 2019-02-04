ff270fafc3263fd478b4658552885bd03c5a1eb8
matt0418 <mfeldma7@gmail.com>
2019-02-04 17:56:17 -0500

    Completed incrementifodd

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,14 +5,18 @@ import { increment, decrement } from '../actions';
 
 
 class Counter extends Component {
-    // incrementIfOdd = () => {
-    //     // Stretch Problem: Implement an increment function that
-    //     // only increments if the counter value is odd
-    // };
+    incrementIfOdd = () => {
+        // Stretch Problem: Implement an increment function that
+        // only increments if the counter value is odd
+        if (this.props.count % 2 !== 0) {
+            this.props.increment();
+        }
+    };
 
     // incrementAsync = () => {
     //     // Stretch Problem: Implement an increment function that
     //     // increments after waiting for one second
+
     // };
 
 
@@ -38,10 +42,10 @@ class Counter extends Component {
                 </button>
                  {/* Uncomment these button tags if you got
                 around to implementing the extra credit functions */}
-                {/* <button onClick={this.incrementIfOdd}>
+                <button onClick={this.incrementIfOdd}>
                     Increment if odd
                 </button>
-                <button onClick={this.incrementAsync}>
+                {/* <button onClick={this.incrementAsync}>
                     Increment async
                 </button>  */}
             </p>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -10,18 +10,17 @@ const initialState = {
 // by our action creator. What does the reducer
 // need to do with the count in each case?
 export default (state = initialState, action) => {
-  
   switch (action.type) {
     case INCREMENT:
       return {
         ...state,
-        count: state.count + 1
+        count: ++state.count
       }
     // Fill in the body of this case
     case DECREMENT:
       return {
         ...state,
-        count: state.count - 1
+        count: --state.count
       }
     // Fill in the body of this case
     default:
