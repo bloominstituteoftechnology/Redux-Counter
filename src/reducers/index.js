890f02228aaed64d9e7e15d1d7e71ce0f0d08602
jkaddu <jkaddu@yahoo.com>
2019-04-08 14:09:21 -0700

    stretch done

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -12,17 +12,17 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
+    // Fill in the body of this case
       return {
         ...state,
         count: state.count + 1
       }
-    // Fill in the body of this case
     case DECREMENT:
+    // Fill in the body of this case
       return {
         ...state,
         count: state.count - 1
       }
-    // Fill in the body of this case
     default:
       return state;
   }
