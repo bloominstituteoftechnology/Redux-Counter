e2684e4adceb28412847fcb3e21310cf26bad5fb
Louis Magdaleno <buildthatapp@gmail.com>
2019-04-10 09:58:10 -0700

    Updated reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -13,8 +13,12 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
     // Fill in the body of this case
+      state.count ++;
+      return state;
     case DECREMENT:
     // Fill in the body of this case
+      state.count --;
+      return state;
     default:
       return state;
   }
