109346dc4ba221d26e049b2f070636c76c3b8a36
raberin <rmvaberin@gmail.com>
2019-04-10 21:14:56 -0700

    MVP Completed

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,20 +1,20 @@
-import { INCREMENT, DECREMENT } from '../actions';
+import { INCREMENT, DECREMENT } from "../actions";
 
 const initialState = {
   count: 0
-}
+};
 
 // Our reducer that handles our two action cases:
 // increment and decrement. It receives the state
 // of our redux store, along with an action created
 // by our action creator. What does the reducer
 // need to do with the count in each case?
-export default (state = initialState, action) => {
+export default (state = initialState.count, action) => {
   switch (action.type) {
     case INCREMENT:
-    // Fill in the body of this case
+      return (state += 1);
     case DECREMENT:
-    // Fill in the body of this case
+      return (state -= 1);
     default:
       return state;
   }
