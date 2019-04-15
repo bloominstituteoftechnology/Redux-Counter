04b8b2d92148a6cdc72a304a548856e9f0b1b89b
motorcitycode <detropia.com@gmail.com>
2019-04-15 18:17:50 -0400

    completed redux counter mvp

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -9,15 +9,15 @@ const initialState = {
 // of our redux store, along with an action created
 // by our action creator. What does the reducer
 // need to do with the count in each case?
-export default (count = 0, action) => {
+export default (state = 0, action) => {
   switch (action.type) {
     case INCREMENT:
     // Fill in the body of this case
-    return ++count;
+    return ++state;
     case DECREMENT:
     // Fill in the body of this case
-    return --count;
+    return --state;
     default:
-      return count;
+      return state;
   }
 };
