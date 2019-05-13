d7b65ed5992b54ebc43511049528f2b308476d62
Gustavo Isturiz <isturizgustavo@gmail.com>
2019-05-13 17:14:24 -0400

    Increment and decrement buttons functioning correctly

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -9,12 +9,19 @@ const initialState = {
 // of our redux store, along with an action created
 // by our action creator. What does the reducer
 // need to do with the count in each case?
+
 export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
     // Fill in the body of this case
+      return {
+        count: state.count + 1
+      }
     case DECREMENT:
     // Fill in the body of this case
+    return {
+      count: state.count - 1
+    }
     default:
       return state;
   }
