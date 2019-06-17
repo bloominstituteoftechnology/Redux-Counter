cc11059b1624118490de41a067c64959e93450ce
JakeGrath <J.A.McGrath@outlook.com>
2019-06-17 15:43:51 -0500

    Initial commit, finished MVP and stretch

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -13,8 +13,15 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
     // Fill in the body of this case
-    case DECREMENT:
+    return {
+      count: state.count + 1
+    };
     // Fill in the body of this case
+    case DECREMENT:
+        return {
+          count: state.count - 1
+        };
+
     default:
       return state;
   }
