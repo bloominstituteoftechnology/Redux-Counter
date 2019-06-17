77fac7982a382f901f3939ab6e2dc2674a5e346c
Hoxtygen <hoxtygen@live.com>
2019-06-17 13:58:40 +0100

    Initial commit

--- src/actions/index.js
+++ src/actions/index.js
@@ -11,6 +11,7 @@ export const DECREMENT = 'DECREMENT';
 
 export const increment = () => {
   // Fill in this function
+  
 };
 
 export const decrement = () => {
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -12,9 +12,15 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
+      return {
+        state: state.count + 1
+      }
     // Fill in the body of this case
     case DECREMENT:
     // Fill in the body of this case
+    return {
+      state: state.count - 1
+    }
     default:
       return state;
   }
