07a66cc16f5c9cb066887ed84bbc779a57a687a5
Mariana Huh <mhuh0176@gmail.com>
2019-06-18 22:34:31 -0700

    implemented actions and reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@ import { INCREMENT, DECREMENT } from '../actions';
 
 const initialState = {
   count: 0
-}
+};
 
 // Our reducer that handles our two action cases:
 // increment and decrement. It receives the state
@@ -12,9 +12,15 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
-    // Fill in the body of this case
+      return {
+        ...state,
+        count: state.count + 1
+      };
     case DECREMENT:
-    // Fill in the body of this case
+      return {
+        ...state,
+        count: state.count - 1
+      };
     default:
       return state;
   }
