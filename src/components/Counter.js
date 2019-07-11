42edf5c886218fdff2c05a1a458cc23a278f6dee
Kevin Grant <ConsciousK@yahoo.com>
2019-07-10 21:03:48 -0400

    MVP met. Counter Incrementing via REDUX flow.

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -20,10 +20,13 @@ class Counter extends Component {
         return (
             <p>
                 Clicked: {this.props.count} times
-                <button onClick={() => {/* Fill me in */ }}>
+                <button onClick={() => { 
+                    this.props.increment();
+                    console.log(this.props.count) 
+                }}>
                     +
                 </button>
-                <button onClick={() => {/* Fill me in */ }}>
+                <button onClick={() => { this.props.decrement() }}>
                     -
                 </button>
                  {/* Uncomment these button tags if you got
@@ -46,6 +49,7 @@ class Counter extends Component {
 // redux application, though, it would receive only the relevant
 // parts it needs from the state object.
 const mapStateToProps = (state) => {
+    console.log('mapStateToProps', state);
     return {
         count: state.count
     };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -10,11 +10,14 @@ const initialState = {
 // by our action creator. What does the reducer
 // need to do with the count in each case?
 export default (state = initialState, action) => {
+  console.log("state", state);
   switch (action.type) {
     case INCREMENT:
-    // Fill in the body of this case
+      const upCount = ++state.count;
+      return {count: upCount};
     case DECREMENT:
-    // Fill in the body of this case
+      const downCount = --state.count;
+      return {count: downCount};
     default:
       return state;
   }
