dd1ee570539221fdb5e925cd2995ff308d92fe05
Juan Aleman <juan.j.aleman@gmail.com>
2019-07-10 20:14:16 -0500

    filled in function for reducers.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -12,8 +12,22 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
+      const count = action.payload
+      const newCount = parseInt(count) + 1
+
+      return {
+        ...state,
+        [count]: newCount,
+      }
     // Fill in the body of this case
     case DECREMENT:
+        const count = action.payload
+        const newCount = parseInt(count) - 1
+  
+        return {
+          ...state,
+          [count]: newCount,
+        }
     // Fill in the body of this case
     default:
       return state;
